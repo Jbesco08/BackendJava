2d9d17aebb3aff3a2bfecde5e76bafd627e4ee28
Jesus_Barrios <jbescorcia@outlook.es>
2022-10-21 23:35:45 -0500

    math practica

--- DataTypeJava/src/MathOperation.java
+++ DataTypeJava/src/MathOperation.java
@@ -1,3 +1,4 @@
+import java.math.*;
 public class MathOperation {
     public static void main(String[] args) {
         double x = 2.1, y = 3;
@@ -24,5 +25,16 @@ public class MathOperation {
         //volumen de una esfera
         // (4/3)*PI * 3
         System.out.println((4/3)*Math.PI* Math.pow(y,3));
+
+
+        // practica conversion de expresiones
+
+        System.out.println((3/2)+(4/3));
+        System.out.println((1/x-3)- 3*x*y/4);
+        System.out.println((1/2) + 7);
+        System.out.println(7+(1/2));
+        System.out.println(Math.pow(x,2) / x -y);
+
+
     }
 }
