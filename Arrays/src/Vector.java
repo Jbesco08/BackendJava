7b4575af308e5577071bc22d1a2c613e1aafb769
jesus_Barrios <Jbescorcia@outlook.es>
2022-10-29 11:53:56 -0500

    java

---
+++ Arrays/src/Vector.java
@@ -0,0 +1,21 @@
+public class Vector {
+    public static void main(String[] args) {
+
+        String [] androidVersions = new String[17];
+        String [] days = new String[7];
+
+        String [][] cities = new String[4][2];
+        /*
+         * +------------------------------+
+         * |  Country    |  City          |
+         * --------------------------------
+         * | México      | CDMX           |
+         * | México      | Guadalajara    |
+         * | Colombia    | Bogotá         |
+         * | Colombia    | Medellín       |
+         * +------------------------------+
+         * */
+
+    }
+
+}
